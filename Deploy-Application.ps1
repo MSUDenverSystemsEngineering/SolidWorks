cc97728d95d7f93fafc7eb4b767ee3812f51f5ab
quantranmsu <tranqua@msudenver.edu>
2018-07-23 13:05:00 -0600

    Updated ps1

--- Deploy-Application.ps1
+++ Deploy-Application.ps1
@@ -1,6 +1,11 @@
 ﻿<#
 .SYNOPSIS
 	This script performs the installation or uninstallation of an application(s).
+	# LICENSE #
+	PowerShell App Deployment Toolkit - Provides a set of functions to perform common application deployment tasks on Windows.
+	Copyright (C) 2017 - Sean Lillis, Dan Cunningham, Muhammad Mashwani, Aman Motazedian.
+	This program is free software: you can redistribute it and/or modify it under the terms of the GNU Lesser General Public License as published by the Free Software Foundation, either version 3 of the License, or any later version. This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for more details.
+	You should have received a copy of the GNU Lesser General Public License along with this program. If not, see <http://www.gnu.org/licenses/>.
 .DESCRIPTION
 	The script is provided as a template to perform an install or uninstall of an application(s).
 	The script either performs an "Install" deployment type or an "Uninstall" deployment type.
@@ -29,10 +34,12 @@
 	60000 - 68999: Reserved for built-in exit codes in Deploy-Application.ps1, Deploy-Application.exe, and AppDeployToolkitMain.ps1
 	69000 - 69999: Recommended for user customized exit codes in Deploy-Application.ps1
 	70000 - 79999: Recommended for user customized exit codes in AppDeployToolkitExtensions.ps1
-.LINK 
+.LINK
 	http://psappdeploytoolkit.com
 #>
 [CmdletBinding()]
+## Suppress PSScriptAnalyzer errors for not using declared variables during AppVeyor build
+[Diagnostics.CodeAnalysis.SuppressMessageAttribute("PSUseDeclaredVarsMoreThanAssignments", "", Justification="Suppresses AppVeyor errors on informational variables below")]
 Param (
 	[Parameter(Mandatory=$false)]
 	[ValidateSet('Install','Uninstall')]
@@ -50,42 +57,42 @@ Param (
 
 Try {
 	## Set the script execution policy for this process
-	Try { Set-ExecutionPolicy -ExecutionPolicy 'ByPass' -Scope 'Process' -Force -ErrorAction 'Stop' } Catch {}
-	
+	Try { Set-ExecutionPolicy -ExecutionPolicy 'ByPass' -Scope 'Process' -Force -ErrorAction 'Stop' } Catch { Write-Error "Failed to set the execution policy to Bypass for this process." }
+
 	##*===============================================
 	##* VARIABLE DECLARATION
 	##*===============================================
 	## Variables: Application
-	[string]$appVendor = ''
-	[string]$appName = 'SolidWorks'
-	[string]$appVersion = '2017 SP3'
+	[string]$appVendor = 'SolidWorks'
+	[string]$appName = '2018'
+	[string]$appVersion = 'SP3'
 	[string]$appArch = ''
 	[string]$appLang = 'EN'
 	[string]$appRevision = '01'
-	[string]$appScriptVersion = '1.0.0'
-	[string]$appScriptDate = '05/17/2017'
-	[string]$appScriptAuthor = 'Quan Tran'
+	[string]$appScriptVersion = '3.7.0.1'
+	[string]$appScriptDate = '06/22/2018'
+	[string]$appScriptAuthor = 'MSU Denver'
 	##*===============================================
 	## Variables: Install Titles (Only set here to override defaults set by the toolkit)
 	[string]$installName = ''
 	[string]$installTitle = ''
-	
+
 	##* Do not modify section below
 	#region DoNotModify
-	
+
 	## Variables: Exit Code
 	[int32]$mainExitCode = 0
-	
+
 	## Variables: Script
 	[string]$deployAppScriptFriendlyName = 'Deploy Application'
-	[version]$deployAppScriptVersion = [version]'3.6.9'
-	[string]$deployAppScriptDate = '02/12/2017'
+	[version]$deployAppScriptVersion = [version]'3.7.0'
+	[string]$deployAppScriptDate = '02/13/2018'
 	[hashtable]$deployAppScriptParameters = $psBoundParameters
-	
+
 	## Variables: Environment
 	If (Test-Path -LiteralPath 'variable:HostInvocation') { $InvocationInfo = $HostInvocation } Else { $InvocationInfo = $MyInvocation }
 	[string]$scriptDirectory = Split-Path -Path $InvocationInfo.MyCommand.Definition -Parent
-	
+
 	## Dot source the required App Deploy Toolkit Functions
 	Try {
 		[string]$moduleAppDeployToolkitMain = "$scriptDirectory\AppDeployToolkit\AppDeployToolkitMain.ps1"
@@ -98,51 +105,58 @@ Try {
 		## Exit the script, returning the exit code to SCCM
 		If (Test-Path -LiteralPath 'variable:HostInvocation') { $script:ExitCode = $mainExitCode; Exit } Else { Exit $mainExitCode }
 	}
-	
+
 	#endregion
 	##* Do not modify section above
 	##*===============================================
 	##* END VARIABLE DECLARATION
 	##*===============================================
-		
+
 	If ($deploymentType -ine 'Uninstall') {
 		##*===============================================
 		##* PRE-INSTALLATION
 		##*===============================================
 		[string]$installPhase = 'Pre-Installation'
-		
-		## Show Welcome Message, close Internet Explorer if required, allow up to 3 deferrals, verify there is enough disk space to complete the install, and persist the prompt
-		Show-InstallationWelcome -CheckDiskSpace -PersistPrompt
-		
+
+		## Show Welcome Message, close Internet Explorer if needed, verify there is enough disk space to complete the install, and persist the prompt
+		Show-InstallationWelcome -CloseApps 'iexplore' -CheckDiskSpace -PersistPrompt
+
 		## Show Progress Message (with the default message)
 		Show-InstallationProgress
-		
+
 		## <Perform Pre-Installation tasks here>
-		
-		
+
+
 		##*===============================================
-		##* INSTALLATION 
+		##* INSTALLATION
 		##*===============================================
 		[string]$installPhase = 'Installation'
-		
+
 		## Handle Zero-Config MSI Installations
 		If ($useDefaultMsi) {
 			[hashtable]$ExecuteDefaultMSISplat =  @{ Action = 'Install'; Path = $defaultMsiFile }; If ($defaultMstFile) { $ExecuteDefaultMSISplat.Add('Transform', $defaultMstFile) }
 			Execute-MSI @ExecuteDefaultMSISplat; If ($defaultMspFiles) { $defaultMspFiles | ForEach-Object { Execute-MSI -Action 'Patch' -Path $_ } }
 		}
-		
+
 		## <Perform Installation tasks here>
-		Execute-Process -Path "$dirFiles\startswinstall.exe" -Parameters "/install /now" -WindowStyle 'Hidden' -IgnoreExitCodes '3010'
-		
+
+		$exitCode = Execute-Process -Path "$dirFiles\startswinstall.exe" -Parameters "/install /now" -WindowStyle "Hidden" -PassThru
+        If (($exitCode.ExitCode -ne "0") -and ($mainExitCode -ne "3010")) { $mainExitCode = $exitCode.ExitCode }
+
+       ## $exitCode = Execute-Process -Path "Setup.exe" -Parameters "/v`"/qn REBOOT=reallysuppress`"" -WindowStyle "Hidden" -PassThru
+        ##If (($exitCode.ExitCode -ne "0") -and ($mainExitCode -ne "3010")) { $mainExitCode = $exitCode.ExitCode }
+
 		##*===============================================
 		##* POST-INSTALLATION
 		##*===============================================
 		[string]$installPhase = 'Post-Installation'
-		
+
 		## <Perform Post-Installation tasks here>
-		
+
 		## Display a message at the end of the install
-		If (-not $useDefaultMsi) {}
+		If (-not $useDefaultMsi) {
+
+		}
 	}
 	ElseIf ($deploymentType -ieq 'Uninstall')
 	{
@@ -150,66 +164,77 @@ Try {
 		##* PRE-UNINSTALLATION
 		##*===============================================
 		[string]$installPhase = 'Pre-Uninstallation'
-		
+
 		## Show Welcome Message, close Internet Explorer with a 60 second countdown before automatically closing
 		Show-InstallationWelcome -CloseApps 'sldProdMon,SLDWORKS,sldworks_fs' -silent
-		
+
 		## Show Progress Message (with the default message)
 		Show-InstallationProgress
-		
+
 		## <Perform Pre-Uninstallation tasks here>
-		
-		
+
+
 		##*===============================================
 		##* UNINSTALLATION
 		##*===============================================
 		[string]$installPhase = 'Uninstallation'
-		
+
 		## Handle Zero-Config MSI Uninstallations
 		If ($useDefaultMsi) {
 			[hashtable]$ExecuteDefaultMSISplat =  @{ Action = 'Uninstall'; Path = $defaultMsiFile }; If ($defaultMstFile) { $ExecuteDefaultMSISplat.Add('Transform', $defaultMstFile) }
 			Execute-MSI @ExecuteDefaultMSISplat
 		}
-		
-		# <Perform Uninstallation tasks here>
-		#Uninstall SOLIDWORKS 2017 SP03
-		Execute-MSI -Action Uninstall -Path '{BB965FD0-077F-4CA4-BFD1-39FFEFF15770}'
 
-		#Uninstall SOLIDWORKS eDrawings 2017 SP03
-		Execute-MSI -Action Uninstall -Path '{004F702B-4455-4132-9DC2-2E82263A3E06}'
+		# <Perform Uninstallation tasks here>
+		#Uninstall SOLIDWORKS 2018 SP03
+		Execute-MSI -Action Uninstall -Path '{837A0B0D-F508-4088-8B05-606477DEB905}'
+
+		#Uninstall SOLIDWORKS eDrawings 2018 SP03
+		Execute-MSI -Action Uninstall -Path '{995E8013-00B1-4F8F-BA13-FF96C1B5DFBB}'
 		
-		#Uninstall SOLIDWORKS Composer Player 2017 SP03
-		Execute-MSI -Action Uninstall -Path '{2F5D372A-EE3F-4201-8899-AA717AB91110}'
+		#Uninstall SOLIDWORKS Composer Player 2018 SP03
+		Execute-MSI -Action Uninstall -Path '{DED607E3-AA70-4A76-A7FD-56124754A762}'
 
 		#Uninstall SOLIDWORKS PDM Client
-		Execute-MSI -Action Uninstall -Path '{44C80B40-F98A-4CFF-84AC-7984F6F8411C}'
+		Execute-MSI -Action Uninstall -Path '{43CE84D6-3110-42FE-8907-85C5FB75D6D8}'
 
-		#Uninstall SOLIDWORKS Electrical2017 SP03
-		Execute-MSI -Action Uninstall -Path '{B4DEFB17-F6CA-4FBD-8FE3-2D22D6F7186B}'
+		#Uninstall SOLIDWORKS Electrical2018 SP03
+		Execute-MSI -Action Uninstall -Path '{F5280967-D8E8-4386-B4E0-62937CFE1970}'
 
-		#Uninstall SOLIDWORKS Plastics 2017 SP03
+		#Uninstall SOLIDWORKS Plastics 2018 SP03
 		Execute-MSI -Action Uninstall -Path '{BB73823B-4D4D-44FF-86A3-4CDC21CF76F0}'
 
-		#Uninstall SOLIDWORKS Flow Simulation 2017 SP03
-		Execute-MSI -Action Uninstall -Path '{C69508D6-6335-49D1-A206-50DCA9A166AE}'
+		#Uninstall SOLIDWORKS Flow Simulation 2018 SP03
+		Execute-MSI -Action Uninstall -Path '{984CA261-0340-4C4F-AC6E-FE4890B5D6DF}'
 
-		#Uninstall SOLIDWORKS Visualize 2017 SP03
-		Execute-MSI -Action Uninstall -Path '{D802560E-F2DA-4CCD-85EB-C29437F112F2}'
+		#Uninstall SOLIDWORKS Visualize 2018 SP03
+		Execute-MSI -Action Uninstall -Path '{FA9DFD3D-B786-491F-8195-8320FA7D2425}'
+
+		#Uninstall SOLIDWORKS Plastics 2018 SP03
+		Execute-MSI -Action Uninstall -Path '{9F301E50-ED1C-408F-85AC-D182E400F61B}'
+
+		#Uninstall SOLIDWORKS Manage Client 2018 SP03
+		Execute-MSI -Action Uninstall -Path '{BCCEB20C-871D-4672-9504-42C58BDBC050}'
+
+		#Uninstall SOLIDWORKS CAM 2018 SP03
+		Execute-MSI -Action Uninstall -Path '{C9043FEF-A6CE-4725-8A93-1488DF0335DF}'
+
+
 
 		##*===============================================
 		##* POST-UNINSTALLATION
 		##*===============================================
 		[string]$installPhase = 'Post-Uninstallation'
-		
+
 		## <Perform Post-Uninstallation tasks here>
-		
-		
+
+
 	}
-	
+
 	##*===============================================
 	##* END SCRIPT BODY
 	##*===============================================
-	
+
 	## Call the Exit-Script function to perform final cleanup operations
 	Exit-Script -ExitCode $mainExitCode
 }
